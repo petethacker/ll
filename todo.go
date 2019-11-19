1e1667d146f0b09c6a47b5fe13b27ac17d565c3c
Peter Thacker <petert@nvidia.com>
2019-11-19 15:02:28 +0000

    updating the todo

--- todo.go
+++ todo.go
@@ -3,5 +3,5 @@ package main
 func todo() {
 	print("change RemoveArgs() for the better")
 	print("cleanup ... lots")
-	print("recurse directories functionallity")
+	print("recurse directories functionallity partly done. doesnt do symlinks and needs better formatting")
 }
